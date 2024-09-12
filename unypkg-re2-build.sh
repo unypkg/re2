647d119c90db35cae76e06e6b44b3477d3e15923
michacassola <mich@casso.la>
2024-09-12 17:17:39 +0200

    Update unypkg-re2-build.sh

--- unypkg-re2-build.sh
+++ unypkg-re2-build.sh
@@ -11,7 +11,7 @@ set -vx
 wget -qO- uny.nu/pkg | bash -s buildsys
 
 ### Installing build dependencies
-unyp install abseil-cpp cmake pcre python icu
+unyp install abseil-cpp/20240116.2 cmake pcre python icu
 
 #pip3_bin=(/uny/pkg/python/*/bin/pip3)
 #"${pip3_bin[0]}" install --upgrade pip
